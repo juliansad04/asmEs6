341b2065a43f297900a34b8c032065b7dad02572
thanhnppc <thanhnppc05965@fpt.edu.vn>
2024-02-04 15:21:27 +0700

    add_category-admin

---
+++ admin/js/add-category.js
@@ -0,0 +1,50 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import {
+    getDatabase,
+    ref,
+    push,
+    set,
+    get,
+    child,
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+import {
+    getStorage,
+    ref as storageRef,
+    uploadBytes,
+    getDownloadURL,
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBPWp2ha8zD9ZnbOv7dSbSELGCe86iytAY",
+    authDomain: "thanhnppcecma.firebaseapp.com",
+    databaseURL:
+        "https://thanhnppcecma-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "thanhnppcecma",
+    storageBucket: "thanhnppcecma.appspot.com",
+    messagingSenderId: "96233696774",
+    appId: "1:96233696774:web:b47da30031cc6231269d87",
+    storageBucket: "thanhnppcecma.appspot.com",
+};
+
+initializeApp(firebaseConfig);
+
+const dbRefCategory = ref(getDatabase(), "category");
+
+window.addCategory = async function (event) {
+    event.preventDefault();
+
+    const categoriesRef = ref(getDatabase(), "category");
+    const newCategoryRef = push(categoriesRef);
+
+    const categoryData = {
+        id: newCategoryRef.key,
+        name: document.getElementById("categoryName").value,
+    };
+
+    set(newCategoryRef, categoryData);
+
+    window.location.href = "category.html";
+
+    console.log(categoryData);
+    console.log("Category added successfully!");
+};
